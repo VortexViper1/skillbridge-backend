f2c227ca8ba6f75c4a71c9f41cc86350bd034c99
Neerumalla Himavath Sai Ganesh <ganesh.neerumalla@gmail.com>
2026-07-02 15:40:15 +0530

    Refactor database connection URL to use environment variable
    Updated DATABASE_URL to use environment variable instead of hardcoded value.

--- database.py
+++ database.py
@@ -1,12 +1,11 @@
+import os
 from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import declarative_base, sessionmaker
 
-DATABASE_URL = "sqlite:///./skillbridge.db"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(
-    DATABASE_URL,
-    connect_args={"check_same_thread": False}
+    DATABASE_URL
 )
 
 SessionLocal = sessionmaker(
